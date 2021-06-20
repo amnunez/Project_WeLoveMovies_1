32aeabbb682144a7fa3026fc85865cb097ac4367
amnunez <adrian@amndsgn.com>
2021-06-20 18:31:03 -0500

    initial commit - complete project

---
+++ knexfile.js
@@ -0,0 +1,47 @@
+const path = require("path");
+require("dotenv").config();
+if (process.env.USER) require("dotenv").config();
+
+const {
+  DATABASE_URL = "postgres://kdubqzuv:PrcDMlM_eyNgAkmkWXkXHZjya87D1aUl@ruby.db.elephantsql.com:5432/kdubqzuv",
+} = process.env;
+
+module.exports = {
+  development: {
+    client: "postgresql",
+    connection: DATABASE_URL,
+    pool: { min: 0, max: 5 },
+    migrations: {
+      directory: path.join(__dirname, "src", "db", "migrations"),
+    },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"),
+    },
+  },
+
+  production: {
+    client: "postgresql",
+    connection: DATABASE_URL,
+    pool: { min: 0, max: 5 },
+    migrations: {
+      directory: path.join(__dirname, "src", "db", "migrations"),
+    },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"),
+    },
+  },
+
+  test: {
+    client: "sqlite3",
+    connection: {
+      filename: ":memory:",
+    },
+    migrations: {
+      directory: path.join(__dirname, "src", "db", "migrations"),
+    },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"),
+    },
+    useNullAsDefault: true,
+  },
+};
